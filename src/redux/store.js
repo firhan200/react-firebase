cbb019a453d89c4898dc5568637468ca46bd1595
root <root@DESKTOP-COCVU1D.localdomain>
2019-04-29 09:54:14 +0700

    disabled redux devtools dependency

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 export default createStore(rootReducer, compose(applyMiddleware(thunk)
     // , window.devToolsExtension ? window.devToolsExtension() : f => f
